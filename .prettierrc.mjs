66ddd17a979f810a6fa9d40eaa1e8d3463b40b91
saya <saya.wow@outlook.com>
2023-09-04 20:27:25 +0800

    react template

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: "none",
+  tabWidth: 2
+};
+
+export default config;
---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+const App = () => {
+  return <p>Have fun!</p>;
+};
+export default App;
